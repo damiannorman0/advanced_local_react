7c9b9e2679477dfba910c9776c040a28ba82fff6
Damian Norman <damiannorman@gmail.com>
2020-03-26 14:02:03 -0400

    rm unecessry catch

--- src/utils.js
+++ src/utils.js
@@ -2,16 +2,12 @@ export function getTODO() {
 	return fetch('https://jsonplaceholder.typicode.com/todos')
 		.then(response => {
 			return response.json();
-		}).catch(error => {
-			return error;
 		});
-};
+}
 
 export function getSW() {
 	return fetch('https://swapi.co/api/people/')
 		.then(response => {
 			return response.json();
-		}).catch(error => {
-			return error;
 		});
-};
+}
