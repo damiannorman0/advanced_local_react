e79b108ab0841b8f93c84e78d961dfa23e56eb81
Damian Norman <damiannorman@gmail.com>
2020-04-09 21:33:51 -0400

    abs paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import {getTODO, getSW} from './utils';
+import {getTODO, getSW} from 'utils';
 
 class App extends Component {
 	constructor(props) {
